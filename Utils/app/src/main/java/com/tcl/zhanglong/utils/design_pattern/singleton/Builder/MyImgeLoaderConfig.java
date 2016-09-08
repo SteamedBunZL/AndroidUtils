3991b43e80b89ce051bd856f7831e4b1a151cea7
Stevezhang <zhang.19901990@163.com>
2016-09-08 09:18:51 +0800

    add some changes

--- Utils/app/src/main/java/com/tcl/zhanglong/utils/design_pattern/singleton/Builder/MyImgeLoaderConfig.java
+++ Utils/app/src/main/java/com/tcl/zhanglong/utils/design_pattern/singleton/Builder/MyImgeLoaderConfig.java
@@ -3,6 +3,6 @@ package com.tcl.zhanglong.utils.design_pattern.singleton.builder;
 /**
  * Created by Steve on 16/9/8.
  */
-public class MyImageLoader {
-    private MyImgeLoaderConfig mConfig;
+public class MyImgeLoaderConfig {
+
 }
